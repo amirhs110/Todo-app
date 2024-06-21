8b9fbd3bb0120d2cba8fb23597d4d50aac79bdbc
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-06-22 03:08:13 +0330

    add dashboard page and use ListView
    + add Dashboard template
    + use ListView to show Tasks

--- core/task/urls.py
+++ core/task/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, TaskListView
 
 app_name = 'task'
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
+    path('dashboard/', TaskListView.as_view(), name="dashboard")
 ]
--- core/task/views.py
+++ core/task/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Task
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+class TaskListView(ListView):
+    template_name = 'task/dashboard.html'
+    queryset= Task.objects.all()
+    paginate_by = 6
+    ordering = "-id"
