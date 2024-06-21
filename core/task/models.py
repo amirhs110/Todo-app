920d109c51368d36747c44791735fbcfd6ece67a
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-06-22 02:32:12 +0330

    add task model in task app

--- core/task/models.py
+++ core/task/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=True,blank=True)
+    complete = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
