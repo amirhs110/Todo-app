30d07c37397230e7d8f03b417a310753cb51b606
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-06-22 02:37:32 +0330

    add admin settings for Task model

--- core/task/admin.py
+++ core/task/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+    list_display=('user', 'title', 'created_date', 'complete')
+    list_filter=('user', 'complete')
+    search_fields=('title', 'user')
+    ordering=('-created_date',)
+
+admin.site.register(Task,TaskAdmin)
