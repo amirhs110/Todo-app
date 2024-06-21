5bf75b4f86979e8db9d611117b646f898a5cb20c
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-06-21 23:15:15 +0330

    create a Custom User Manager
    create CustomUserManager for my User model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,8 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    '''
+        This is a Custom User Manager that manage create a user and superuser with imported user's data
+    '''
+    def create_user(self, email, password, first_name, last_name, **extra_fields):
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        first_name = first_name.strip()
+        last_name= last_name.strip()
+        user = self.model(email=email,first_name=first_name,last_name=last_name,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser,PermissionsMixin):
 
@@ -17,6 +45,8 @@ class User(AbstractBaseUser,PermissionsMixin):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['first_name','last_name']
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
