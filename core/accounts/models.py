043f53c8ac60140347c87d3abed2b9683854370c
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-06-21 22:38:49 +0330

    Create Custom User Model by AbstractBaseUser

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class User(AbstractBaseUser,PermissionsMixin):
+
+    email = models.EmailField(_("email address"), unique=True, max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['first_name','last_name']
+
+    def __str__(self):
+        return self.email
