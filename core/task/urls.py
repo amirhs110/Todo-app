8627939776b497ee43cdc3e4cad0e28456273137
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-06-22 05:16:01 +0330

    add detail page and create task form

--- core/task/urls.py
+++ core/task/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomeView, TaskListView
+from .views import HomeView, TaskListView, TaskDetailView, TaskCreateView
 
 app_name = 'task'
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('dashboard/', TaskListView.as_view(), name="dashboard")
+    path('dashboard/', TaskListView.as_view(), name="dashboard"),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name="detail"),
+    path('task/create/', TaskCreateView.as_view(), name="task-create"),
 ]
--- core/task/views.py
+++ core/task/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from .models import Task
 
 # Create your views here.
@@ -8,7 +9,23 @@ class HomeView(TemplateView):
     template_name = "home.html"
 
 class TaskListView(ListView):
+    model = Task
     template_name = 'task/dashboard.html'
-    queryset= Task.objects.all()
     paginate_by = 6
     ordering = "-id"
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class TaskDetailView(DetailView):
+    model= Task
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields=['title', 'content']
+    success_url = reverse_lazy("task:dashboard")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
