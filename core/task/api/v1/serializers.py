a740b01fb45533eef3e0c81a7983ecd9451d0c79
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-07-12 22:12:43 +0330

    update TaskSerializer
    add create func to get user fom request

--- core/task/api/v1/serializers.py
+++ core/task/api/v1/serializers.py
@@ -5,3 +5,8 @@ class TaskSerializer(ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'user', 'title', 'content', 'complete', 'created_date', 'updated_date']
+        read_only_fields = ['id' , 'user', 'created_date','updated_date', 'complete']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context.get('request').user
+        return super().create(validated_data)
