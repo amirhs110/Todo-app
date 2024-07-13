935c7a0b4d943339fcfbb21f102bec80aa8d2d69
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-07-13 22:14:36 +0330

    edit api information

--- core/core/urls.py
+++ core/core/urls.py
@@ -27,12 +27,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Todo App",
       default_version='v1',
-      description="Test description",
+      description="This is a project for advanced django course in Maktabkhooneh",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(email="amir.sigari.110@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
