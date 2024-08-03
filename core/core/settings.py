74aebd0a461308521753af85ebf328ac58ecdf83
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-08-03 23:42:45 +0330

    add simpleJwt for using JWT authentication

--- core/core/settings.py
+++ core/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 
     'accounts',
     'task',
@@ -153,6 +154,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
