2a93a998ebfe15824d12fe9c6ff5556a9a3bc01e
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-08-11 01:44:24 +0330

    reformat codes with black and check with flake8

--- core/task/api/v1/views.py
+++ core/task/api/v1/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+
 # from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
--- core/task/management/commands/insert_data.py
+++ core/task/management/commands/insert_data.py
@@ -1,23 +1,25 @@
 import random
 from django.core.management.base import BaseCommand
 from faker import Faker
-from accounts.models import User,Profile
+from accounts.models import User, Profile
 from task.models import Task
+
 
 class Command(BaseCommand):
     """
-        this command when run by "python manage.py insert_data" create a user and user_profile
-        and create 5 task with fake and random data 
+    this command when run by "python manage.py insert_data" create a user and user_profile
+    and create 5 task with fake and random data
     """
-    help='inserting fake data'
 
-    def __init__(self,*args, **kwargs):
-        super(Command,self).__init__(*args, **kwargs)
+    help = "inserting fake data"
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
         self.fake = Faker()
 
     def handle(self, *args, **options):
         # create user instance & get it's profile
-        user = User.objects.create_user(email=self.fake.email(), password='123456@mir')
+        user = User.objects.create_user(email=self.fake.email(), password="123456@mir")
         user_profile = Profile.objects.get(user=user)
         # update user profile with given data
         user_profile.first_name = self.fake.first_name()
@@ -30,11 +32,9 @@ class Command(BaseCommand):
         number_of_tasks = 5
         for _ in range(number_of_tasks):
             Task.objects.create(
-                user = user,
-                title = self.fake.paragraph(nb_sentences=1),
-                content = self.fake.paragraph(nb_sentences=5),
-                complete = random.choice([True,False]),
+                user=user,
+                title=self.fake.paragraph(nb_sentences=1),
+                content=self.fake.paragraph(nb_sentences=5),
+                complete=random.choice([True, False]),
                 # set random status for complete of task
             )
-        
-        
