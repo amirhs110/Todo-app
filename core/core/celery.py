2a7e057b534e5fce3aad60c1b6d12fcd5a6e7a2c
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-08-20 19:37:18 +0330

    add periodic task setup in celery file
    + add this setup to create a task that calls send_email_celery function every 10 seconds.

--- core/core/celery.py
+++ core/core/celery.py
@@ -1,8 +1,16 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+from task.tasks import send_email_celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 app = Celery("core")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls send_email_celery() every 10 seconds.
+    sender.add_periodic_task(10.0, send_email_celery.s(), name='send email every 10')
+
+    # celery -A core beat
