7309399ecfa4210ce46dbd0ec0f968ebfcda8a75
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-08-25 01:20:28 +0330

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -179,7 +179,6 @@ FRONTEND_URL = "http://127.0.0.1:8000"
 
 # Celery settings
 CELERY_BROKER_URL = "redis://redis:6379/1"
-CELERY_RESULT_BACKEND = "redis://redis:6379"
 
 # cache settings to use redis
 CACHES = {
