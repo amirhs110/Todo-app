bcaeb7ea213f0ba989accad000b20b9c2b2e03c0
Amir Sigari <75300999+amirhs110@users.noreply.github.com>
2024-08-26 02:26:35 +0330

    add postgresql for database in docker-compose
    + add psycopg2-binary in requirements
    + create docker compose stage to test before development
    + add database configs in settings.py
    + update .env file

--- core/core/settings.py
+++ core/core/settings.py
@@ -86,12 +86,24 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": config("SQL_ENGINE"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
     }
 }
+
 
 
 # Password validation
